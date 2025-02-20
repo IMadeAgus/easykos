ac0ce148c46a8de9c2738b35e3c5dd8415446f7e
IMadeAgus <agsmade@gmail.com>
2025-02-20 20:17:54 +0800

    add find kos view and booking status view

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -5,11 +5,11 @@
                 <img src="assets/images/icons/global-green.svg" class="w-8 h-8 flex shrink-0" alt="icon">
                 <span class="font-semibold text-sm text-white">Discover</span>
             </a>
-            <a href="check-booking.html" class="flex flex-col items-center text-center gap-2">
+            <a href="{{ route('check-booking') }}" class="flex flex-col items-center text-center gap-2">
                 <img src="assets/images/icons/note-favorite.svg" class="w-8 h-8 flex shrink-0" alt="icon">
                 <span class="font-semibold text-sm text-white">Orders</span>
             </a>
-            <a href="find-kos.html" class="flex flex-col items-center text-center gap-2">
+            <a href="{{ route('find-kos') }}" class="flex flex-col items-center text-center gap-2">
                 <img src="assets/images/icons/search-status.svg" class="w-8 h-8 flex shrink-0" alt="icon">
                 <span class="font-semibold text-sm text-white">Find</span>
             </a>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\BoardingHouseController;
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/check-booking', [BookingController::class, 'check'])->name('check-booking');
+Route::get('/find-kos', [BoardingHouseController::class, 'find'])->name('find-kos');
